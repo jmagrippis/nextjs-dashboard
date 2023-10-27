2cbd2a658c35eef31d83607d714949ff3d299e09
Johnny Magrippis <j@magrippis.com>
2023-10-27 17:54:48 +0200

    ✨  complete chapter 9

--- src/ui/dashboard/revenue-chart.tsx
+++ src/ui/dashboard/revenue-chart.tsx
@@ -1,6 +1,6 @@
 import {generateYAxis} from '@/lib/utils'
 import {CalendarIcon} from '@heroicons/react/24/outline'
-import {Revenue} from '@/lib/definitions'
+import {fetchRevenue} from '@/lib/data'
 
 // This component is representational only.
 // For data visualization UI, check out:
@@ -8,7 +8,9 @@ import {Revenue} from '@/lib/definitions'
 // https://www.chartjs.org/
 // https://airbnb.io/visx/
 
-export default async function RevenueChart({revenue}: {revenue: Revenue[]}) {
+export default async function RevenueChart() {
+	const revenue = await fetchRevenue()
+
 	const chartHeight = 350
 
 	const {yAxisLabels, topLabel} = generateYAxis(revenue)
